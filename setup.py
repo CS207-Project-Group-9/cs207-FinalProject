a870a052d3840e9f9e0150887a3a507e8a0f0a62
kareenaaahuang <qhuang@g.harvard.edu>
2018-12-10 00:34:36 -0500

    updating package

--- setup.py
+++ setup.py
@@ -3,15 +3,23 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="Bambanta",
-    version="0.0.6",
-    author="Karina Huang, Rong Liu, Rory Maizels",
-    author_email="qhuang@g.harvard.edu, rongliu@g.harvard.edu, rorymaizels@g.harvard.edu",
-    description="An automatic differentiation package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/CS207-Project-Group-9/cs207-FinalProject",
-    packages=setuptools.find_packages("Bambanta"),
-    package_dir = {"":"Bambanta"}
+setuptools.setup(name='Bambanta',
+	version='0.1.1',
+	description='An automatic differentiation package',
+	author='Karina Huang, Rong Liu, Rory Maizels',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=['Bambanta'],
+	install_requires=[
+	  'numpy',
+	  'math',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	url="https://github.com/CS207-Project-Group-9/cs207-FinalProject",
+	license="MIT",
 )
