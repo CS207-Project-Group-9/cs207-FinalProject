3a9c08c972c7f3d7a35e04af6b29ed12da5eebd5
kareenaaahuang <qhuang@g.harvard.edu>
2018-12-09 22:19:31 -0500

    updating setup

--- setup.py
+++ setup.py
@@ -13,10 +13,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/CS207-Project-Group-9/cs207-FinalProject",
     packages=setuptools.find_packages("Bambanta"),
-    package_dir = {"":"Bambanta"},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    package_dir = {"":"Bambanta"}
 )
