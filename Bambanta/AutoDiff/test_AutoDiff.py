90721221302d7aed2572e3b5524f22813dad6eb5
kareenaaahuang <qhuang@g.harvard.edu>
2018-12-09 20:17:30 -0500

    fix update

--- Bambanta/AutoDiff/test_AutoDiff.py
+++ Bambanta/AutoDiff/test_AutoDiff.py
@@ -9,7 +9,7 @@ import pytest
 import numpy as np
 from numpy.testing import assert_array_equal, assert_array_almost_equal, assert_approx_equal
 
-import AutoDiff
+from AutoDiff import AutoDiff
 
 #AD_create_f allows for simultaneous assignment 
 #of AD instances
